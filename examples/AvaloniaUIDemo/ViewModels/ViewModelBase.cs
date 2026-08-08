6b40368e7d2dcb1c935f48a0f0948bd794f3daa3
Junian Triajianto <author@junian.dev>
2026-08-08 07:05:42 +0700

    Add Avalonia UI demo application structure and initial implementation

---
+++ examples/AvaloniaUIDemo/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaUIDemo.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ examples/AvaloniaUIDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaUIDemo.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
